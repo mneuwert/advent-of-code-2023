7376e9ce272954e05298042ef98b03fbcf6f6c50
Michael Neuwert <michael.neuwert@googlemail.com>
2023-12-03 15:12:49 +0100

    Day 03 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,6 +2,12 @@ data class PartNumber(val value: Int, val range: IntRange, val lineIndex: Int)
 
 data class Symbol(val value:Char, val range: IntRange, val lineIndex: Int)
 
+data class Gear(val firstPart: PartNumber, val secondPart: PartNumber) {
+    fun ratio(): Int {
+        return firstPart.value * secondPart.value
+    }
+}
+
 fun main() {
 
     fun processInput(input: List<String>): Pair<List<PartNumber>, List<Symbol>> {
@@ -53,11 +59,27 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val processedInput = processInput(input)
+        val parts = processedInput.first
+        val gearSymbols = processedInput.second.filter { it.value == '*' }
+
+        val gears = mutableListOf<Gear>()
+
+        for (gearSymbol in gearSymbols) {
+            val lineRange = IntRange(gearSymbol.lineIndex-1, gearSymbol.lineIndex+1)
+            val inLineRange = IntRange(gearSymbol.range.first-1, gearSymbol.range.first+1)
+
+            val adjacentParts = parts.filter { it.lineIndex in lineRange && inLineRange.intersect(it.range).isNotEmpty() }
+            if (adjacentParts.size == 2) {
+                gears.add(Gear(adjacentParts[0], adjacentParts[1]))
+            }
+        }
+
+        return gears.sumOf { it.ratio() }
     }
 
     val input = readInput("Day03")
-    val input2 = readInput("Day03_test2")
+    val input2 = readInput("Day03")
     part1(input).println()
     part2(input2).println()
 }

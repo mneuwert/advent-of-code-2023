733244c6b52329481a2998ac18c2a6ff92867f67
Michael Neuwert <michael.neuwert@googlemail.com>
2023-12-03 15:00:26 +0100

    Day 03 part 1 refac

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,69 +1,55 @@
+data class PartNumber(val value: Int, val range: IntRange, val lineIndex: Int)
+
+data class Symbol(val value:Char, val range: IntRange, val lineIndex: Int)
+
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val parts = mutableListOf<Int>()
-
-        // Create a regular expression for finding numbers in the line
+    fun processInput(input: List<String>): Pair<List<PartNumber>, List<Symbol>> {
+        val parts = mutableListOf<PartNumber>()
+        val symbols = mutableListOf<Symbol>()
         val numberRegex = Regex("\\d+")
+        val symbolRegex = Regex("[^\\d.]")
 
-        // Iterate over all lines in the input
         for (lineIndex in 0..<input.size) {
-            // Read all numbers from the line using regular expression and store them in a list
             val line = input[lineIndex]
-            val numbers = numberRegex.findAll(line).map { it.value }.toList()
+            val numbers = numberRegex.findAll(line).map { PartNumber(it.value.toInt(), range = it.range, lineIndex = lineIndex) }.toList()
+            val symbolsInLine = symbolRegex.findAll(line).map { Symbol(it.value.first(), range = it.range, lineIndex = lineIndex) }.toList()
 
-            // Iterate over all numbers in the line
             for (number in numbers) {
-                val adjacentChars = mutableListOf<Char>()
+                parts.add(number)
+            }
 
-                // Get a position of number substring in the line
-                var leftIndex = line.indexOf(number)
-                var rightIndex = leftIndex + number.length - 1
+            for (symbol in symbolsInLine) {
+                symbols.add(symbol)
+            }
 
-                // Get adjacent chars in the same line
-                if (leftIndex > 0) {
-                    leftIndex--
-                    adjacentChars.add(line[leftIndex])
-                }
+        }
+        return Pair(parts, symbols)
+    }
 
-                if (rightIndex < line.length - 1) {
-                    rightIndex++
-                    adjacentChars.add(line[rightIndex])
-                }
+    fun part1(input: List<String>): Int {
 
-                // Get adjacent symbols from the previous line
-                if (lineIndex > 0) {
-                    val previousLine = input[lineIndex - 1]
-                    val adjacentTopSubstring = previousLine.substring(leftIndex..rightIndex)
-                    for(adjacentTopChar in adjacentTopSubstring) {
-                        adjacentChars.add(adjacentTopChar)
-                    }
-                }
+        val processedInput = processInput(input)
+        val parts = processedInput.first
+        val symbols = processedInput.second
 
-                // Get adjacent symbols from the next line
-                if (lineIndex < input.size - 1) {
-                    val nextLine = input[lineIndex + 1]
-                    val adjacentBottomSubstring = nextLine.substring(leftIndex..rightIndex)
-                    for(adjacentBottomChar in adjacentBottomSubstring) {
-                        adjacentChars.add(adjacentBottomChar)
-                    }
-                }
+        val validParts = mutableListOf<PartNumber>()
 
-                // Check if there exactly one adjacent symbols which is not digit and not period
-                val hasAdjacentSymbols = adjacentChars.count { !it.isDigit() && it.toString() != "." } == 1
+        for (part in parts) {
 
-                // If so, then it is valid engine part number
-                if (hasAdjacentSymbols) {
-                    parts.add(number.toInt())
-                }
+            // Get a position of number substring in the line
+            var leftIndex = part.range.first
+            var rightIndex = part.range.last
 
-                println("${number}: ${adjacentChars.joinToString(separator = "")} -> ${hasAdjacentSymbols}")
+            // Get adjacent chars in the same line
+            val lineRange = IntRange(part.lineIndex-1, part.lineIndex+1)
+            val inLineRange = IntRange(leftIndex-1, rightIndex+1)
+            symbols.count { it.lineIndex in lineRange && it.range.first in inLineRange }.takeIf { it == 1}?.let {
+                validParts.add(part)
             }
         }
 
-        println(parts.joinToString(separator = ", "))
-
-        return parts.sum()
+        return validParts.sumOf { it.value }
     }
 
     fun part2(input: List<String>): Int {
@@ -71,6 +57,7 @@ fun main() {
     }
 
     val input = readInput("Day03")
+    val input2 = readInput("Day03_test2")
     part1(input).println()
-    //part2(input).println()
+    part2(input2).println()
 }

b2f84576d21ce4bda7df1e9c257853bdd77208d2
Michael Neuwert <michael.neuwert@googlemail.com>
2023-12-05 21:07:48 +0100

    parse garden plan

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,14 +1,49 @@
+data class RangeMapping(val sourceRange:LongRange, val destinationRange:LongRange)
+data class GardenPlan(val seeds:List<Long>, val mappings:Map<String, List<RangeMapping>>)
+
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parse(input: List<String>): GardenPlan {
+        val mappings = mutableMapOf<String, List<RangeMapping>>()
+        val inputCopy = input.toMutableList()
+
+        // Read seed values from the first line formatted as: seeds: 1 2 3 5
+        val seeds = inputCopy[0].substringAfter("seeds: ").split(" ").map { it.toLong() }
+
+        val mapRegex = Regex("([^\\d]+):\n((\\d)+ (\\d)+ (\\d)+\n)+(\n)*")
+        val mapMatches = mapRegex.findAll(inputCopy.joinToString("\n"))
+        mapMatches.map { it.value }.forEach {
+            val (name, numbers) = it.split(":")
+            val rangeMappingList = mutableListOf<RangeMapping>()
+
+            val ranges = numbers.trim().split("\n")
+            ranges.forEach {
+                val range = it.split(" ").map { it.toLong() }
+                if (range.size == 3) {
+                    val destinationRange = LongRange(range[0], range[0] + range[2])
+                    val sourceRange = LongRange(range[1], range[1] + range[2])
+                    rangeMappingList.add(RangeMapping(sourceRange, destinationRange))
+                }
+            }
+            mappings[name] = rangeMappingList
+        }
+        return GardenPlan(seeds, mappings)
+    }
+
+    fun part1(input: List<String>): Long {
+        val plan = parse(input)
+        val locations = mutableListOf<Long>()
+
+        // TODO:
+
+        return locations.min()
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    val input = readInput("Day05")
+    val input = readInput("Day05_test")
     part1(input).println()
     part2(input).println()
 }

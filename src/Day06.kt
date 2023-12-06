e71d4554ee9ccc0a2e91bd1feec3ccaabc836cb7
Michael Neuwert <michael.neuwert@googlemail.com>
2023-12-06 20:08:27 +0100

    Day 06 part 1 solved

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,7 +1,38 @@
+data class Race(val time:Int, val recordDistance:Int)
 fun main() {
 
+    fun distance(totalTime: Int, chargeTime: Int): Int {
+        val remainingTime = totalTime - chargeTime
+        // speed is equal charge time
+        return remainingTime * chargeTime
+    }
+
+    fun parse(input:List<String>): List<Race> {
+        val races = mutableListOf<Race>()
+        val times = input[0].substringAfter("Time:").trim().split(regex = Regex("\\s+")).map { it.toInt() }
+        val distances = input[1].substringAfter("Distance:").trim().split(regex = Regex("\\s+")).map { it.toInt() }
+
+        times.zip(distances).forEach { races.add(Race(it.first, it.second)) }
+
+        return races
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val races = parse(input)
+        var result = 1
+        races.forEach {
+            var waysToWin = 0
+            // Try to hold charge button each time 1ms longer, to overcome the record distance
+            // Count the ways to win
+            for (x in 1..it.time) {
+                if (distance(it.time, x) > it.recordDistance) {
+                    waysToWin++
+                }
+            }
+            result *= waysToWin
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {

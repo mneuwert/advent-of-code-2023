8404b96dfb0447b51685aecc6b790efce00b1cd7
Michael Neuwert <michael.neuwert@googlemail.com>
2023-12-06 20:16:22 +0100

    Day 06 part 2 solved

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,7 +1,7 @@
-data class Race(val time:Int, val recordDistance:Int)
+data class Race(val time:Long, val recordDistance:Long)
 fun main() {
 
-    fun distance(totalTime: Int, chargeTime: Int): Int {
+    fun distance(totalTime: Long, chargeTime: Long): Long {
         val remainingTime = totalTime - chargeTime
         // speed is equal charge time
         return remainingTime * chargeTime
@@ -9,12 +9,18 @@ fun main() {
 
     fun parse(input:List<String>): List<Race> {
         val races = mutableListOf<Race>()
-        val times = input[0].substringAfter("Time:").trim().split(regex = Regex("\\s+")).map { it.toInt() }
-        val distances = input[1].substringAfter("Distance:").trim().split(regex = Regex("\\s+")).map { it.toInt() }
+        val times = input[0].substringAfter("Time:").trim().split(regex = Regex("\\s+")).map { it.toLong() }
+        val distances = input[1].substringAfter("Distance:").trim().split(regex = Regex("\\s+")).map { it.toLong() }
 
         times.zip(distances).forEach { races.add(Race(it.first, it.second)) }
 
         return races
+    }
+
+    fun parse2(input:List<String>): Race {
+        val time = input[0].substringAfter(":").trim().filter { it.isDigit() }.toLong()
+        val distance = input[1].substringAfter(":").trim().filter { it.isDigit() }.toLong()
+        return Race(time, distance)
     }
 
     fun part1(input: List<String>): Int {
@@ -36,7 +42,17 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val race = parse2(input)
+        var waysToWin = 0
+        // Try to hold charge button each time 1ms longer, to overcome the record distance
+        // Count the ways to win.. Probably you could come up with somewhat more efficient solution though
+        for (x in 1..race.time) {
+            if (distance(race.time, x) > race.recordDistance) {
+                waysToWin++
+            }
+        }
+
+        return waysToWin
     }
 
     val input = readInput("Day06")

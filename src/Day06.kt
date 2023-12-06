1940b15cb9e0dcf93502a79370af89fcd6629bd0
Michael Neuwert <michael.neuwert@googlemail.com>
2023-12-06 21:25:40 +0100

    small updates

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,4 +1,19 @@
-data class Race(val time:Long, val recordDistance:Long)
+import java.lang.Math.*
+import kotlin.system.measureTimeMillis
+
+data class Race(val time:Long, val recordDistance:Long) {
+    // TODO: this is not used in the solution, but it's a nice formula to calculate the number of ways to win
+    // Credits: https://github.com/ephemient
+    fun winCount(): Long {
+        // x * (time - x) > distance
+        // x^2 - time*x < -distance
+        // x^2 - time*x + (time/2)^2 < (time/2)^2 - distance
+        // abs(x - time/2) < sqrt((time/2)^2 - distance)
+        val b = time / 2.0
+        val d = sqrt(b * b - recordDistance.toDouble())
+        return (ceil(b + d - 1) - floor(b - d + 1) + 1).toLong()
+    }
+}
 fun main() {
 
     fun distance(totalTime: Long, chargeTime: Long): Long {
@@ -46,16 +61,44 @@ fun main() {
         var waysToWin = 0
         // Try to hold charge button each time 1ms longer, to overcome the record distance
         // Count the ways to win.. Probably you could come up with somewhat more efficient solution though
-        for (x in 1..race.time) {
-            if (distance(race.time, x) > race.recordDistance) {
-                waysToWin++
+        val timeInMillis = measureTimeMillis {
+            for (x in 1..race.time) {
+                val distance = distance(race.time, x)
+                if (distance > race.recordDistance) {
+                    waysToWin++
+                }
             }
         }
+        println("Time: $timeInMillis")
 
+        return waysToWin
+    }
+
+    fun part2_2(input: List<String>): Int {
+        val race = parse2(input)
+        var waysToWin = 0
+        var startedWinning = false
+        // Try to hold charge button each time 1ms longer, to overcome the record distance
+        // Count the ways to win..
+        val timeInMillis = measureTimeMillis {
+            for (x in race.time downTo 1) {
+                val distance = distance(race.time, x)
+                if (distance > race.recordDistance) {
+                    waysToWin++
+                    startedWinning = true
+                } else if (startedWinning) {
+                    // try to stop counting when charging time is not sufficient to win
+                    break
+                }
+            }
+        }
+        println("Time: $timeInMillis")
         return waysToWin
     }
 
     val input = readInput("Day06")
     part1(input).println()
     part2(input).println()
+    part2_2(input).println()
+
 }

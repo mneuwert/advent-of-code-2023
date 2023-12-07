74126a757e1c7848aae4020dcd59a42ff7c57176
Michael Neuwert <michael.neuwert@googlemail.com>
2023-12-07 20:40:17 +0100

    Day 07 part 1 solved

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,7 +1,88 @@
-fun main() {
+data class Hand(val cards: List<Char>, val bid:Long, val rank: Int): Comparable<Hand> {
+    companion object {
+        val validCards = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+        fun strength(card: Char): Int {
+            return validCards.size - validCards.indexOf(card)
+        }
+        fun parse(input: List<String>): List<Hand> {
+            val hands = mutableListOf<Hand>()
+            input.forEach {
+                it.split(' ').let { parts ->
+                    val cards = parts[0].toCharArray().toList()
+                    val bid = parts[1].toLong()
 
-    fun part1(input: List<String>): Int {
-        return input.size
+                    // Strongest to weakest:
+                    // Five of a kind AAAAA -> 7
+                    // Four of a kind AA8AA -> 6
+                    // Full house: 23332 (3+2) -> 5
+                    // Three of a kind TTT98 -> 4
+                    // Two pairs 23432 -> 3
+                    // One pair A23A4 -> 2
+                    // High card 23456 (all different) -> 1
+                    var rank = 0
+                    var pairs = 0
+                    var triples = 0
+
+                    // Find groups of 5.4.3 and 2
+                    cards.groupBy { it }.forEach {
+                        when(it.value.size) {
+                            5 -> {
+                                rank = 7
+                            }
+                            4 -> {
+                                rank = 6
+                            }
+                            2 -> pairs++
+                            3 -> triples++
+                        }
+                    }
+
+                    if (rank == 0) {
+                        // Now take a closer look at combinations of tuples and triples
+                        if (triples > 0) {
+                            if (pairs > 0) {
+                                // full house
+                                rank = 5
+                            } else {
+                                // three of a kind
+                                rank = 4
+                            }
+                        } else {
+                            when (pairs) {
+                                2 -> rank = 3
+                                1 -> rank = 2
+                                else -> rank = 1
+                            }
+                        }
+                    }
+
+                    hands.add(Hand(cards, bid, rank))
+                }
+            }
+            return hands
+        }
+    }
+
+    override fun compareTo(other: Hand): Int {
+        if (rank != other.rank) {
+            return rank.compareTo(other.rank)
+        } else {
+            // compare cards (first non equal card in the hand is relevant)
+            for (i in 0 until cards.size) {
+                if (cards[i] != other.cards[i]) {
+                    return strength(cards[i]).compareTo(strength(other.cards[i]))
+                }
+            }
+        }
+        return 0
+    }
+}
+fun main() {
+    fun part1(input: List<String>): Long {
+        return Hand.parse(input)
+            .sorted()
+            .mapIndexed { i, h -> h.bid * (i + 1) }
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
